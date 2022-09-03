bacde40806014d1cb265b7eac98a53e057c733be
PerSeMaverick <skhu151827@gmail.com>
2022-09-04 02:21:55 +0900

    automatically updates list, filter list by year
    assignment complite

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -5,7 +5,7 @@ import "./ExpenseDate.css";
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
+  const year = props.date.getFullYear(); // 연도를 출력하는 함수
 
   return (
     <div className="expense-date">
